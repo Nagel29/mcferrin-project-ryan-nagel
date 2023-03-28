26b68263a84620ac7d307dbec34264159f591335
Ryan Nagel <108195380+Nagel29@users.noreply.github.com>
2023-03-28 09:18:45 -0600

    add the initial banner

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
 import Banner from '../Banner/Banner'
 import BeerList from '../BeerList/BeerList'
 import BeerDetails from '../BeerDetails/BeerDetails'
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,25 @@
+import sixPack from "../../assets/beer-box.png"
+import cart from "../../assets/cart.png"
+import './Banner.css'
+
 const Banner = () => {
-  return(<div>banner</div>)
+  return (
+    <nav className="navbar bg-secondary fixed-top text-white">
+      <div className="container-fluid d-flex justify-content-between align-items-end">
+        <div className="d-flex align-items-center">
+          <img
+            src={sixPack}
+            className='logo me-4'
+          />
+          <h1 className='display-1'>Pint Picker</h1>
+        </div>
+        <div className='mx-3'>
+          <img src={cart} className='cart ' />
+          Your Cart
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Banner
